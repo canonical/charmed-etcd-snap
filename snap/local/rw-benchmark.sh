3dcb500df381beb559caa5d0a0ad08b8923d0d1e
Soundarya03 <soundarya1103@gmail.com>
2026-05-05 11:45:49 +0530

    fix: rw-benchmark

--- snap/local/rw-benchmark.sh
+++ snap/local/rw-benchmark.sh
@@ -49,6 +49,7 @@ run_benchmark() {
     --rw-ratio "${ratio}" \
     --limit "${RANGE_RESULT_LIMIT}" \
     --val-size "${value_size}" \
+    --report-interval -1 \
     2>/dev/null | grep "Requests/sec" | awk '{print $2}'
 }
 
